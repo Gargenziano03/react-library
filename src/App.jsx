5f5aa4c27d66c2fd9c7ce7085c6f34a0d2133c8e
Gargenziano03 <gargenziano03@gmail.com>
2024-11-25 10:28:51 +0100

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Books from "./pages/Books"
+import About from "./pages/About"
+import Contacts from "./pages/Contacts"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/about">About</a>
+                    <a href="/contacts">Contats</a>
+                </nav>
+            </header>
+            <main>
+                <h1>home page</h1>
+            </main>
+        </>
+    )
+}
